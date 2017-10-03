3b0ebd80476cf682a17c71193e1237d9788746d8
KyuNam <xmfpes@naver.com>
2017-10-04 00:13:33 +0900

    props를 이용한 컴포넌트로 데이터 전달

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "OldBoy",
+  "KingsMan",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/01/09/the-matrix.jpg",
+  "https://resizing.flixster.com/Mq0_LntBHHLskoj122sGEFh-P2Y=/206x305/v1.bTsxMTE3NjY0NjtqOzE3NTQ0OzEyMDA7MTQwMDsyMTAw",
+  "http://t1.daumcdn.net/liveboard/movie/dc38196dc7824fa083dd5a2b9ebedd8c.JPG",
+  "https://lumiere-a.akamaihd.net/v1/images/lazada-starwars-1-1_741cd5d6.jpeg?region=0%2C0%2C1000%2C1000&width=320"
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie/>
-        <Movie/>
-        <Movie/>
-        <Movie/>
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
       </div>
     );
   }
