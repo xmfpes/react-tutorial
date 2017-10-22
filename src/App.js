9c67d8f14fc1b9895da429671e971e4e933aaea7
KyuNam <xmfpes@naver.com>
2017-10-22 12:28:55 +0900

    css

--- src/App.js
+++ src/App.js
@@ -3,27 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "OldBoy",
-  "KingsMan",
-  "Star Wars"
-]
-
-const movieImages = [
-  "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/01/09/the-matrix.jpg",
-  "https://resizing.flixster.com/Mq0_LntBHHLskoj122sGEFh-P2Y=/206x305/v1.bTsxMTE3NjY0NjtqOzE3NTQ0OzEyMDA7MTQwMDsyMTAw",
-  "http://t1.daumcdn.net/liveboard/movie/dc38196dc7824fa083dd5a2b9ebedd8c.JPG",
-  "https://lumiere-a.akamaihd.net/v1/images/lazada-starwars-1-1_741cd5d6.jpeg?region=0%2C0%2C1000%2C1000&width=320"
-]
 class App extends Component {
+  state = {
+  }
+
+  componentDidMount(){
+   this._getMovies();
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  componentWillMount(){
+    console.log("will mount");
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+        title={movie.title}
+        poster={movie.medium_cover_image} 
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
+    })
+    return movies;
+  }
+
   render() {
+    console.log("render");
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Now Loading...'}
       </div>
     );
   }
